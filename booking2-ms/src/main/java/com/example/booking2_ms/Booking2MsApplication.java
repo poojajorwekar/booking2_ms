83da8237e2995a02a837dbbd903dc526cda8bd35
poojajorwekar <pooja.jorwekar811@gmail.com>
2026-01-14 23:17:50 +0530

    coderefactor

---
+++ booking2-ms/src/main/java/com/example/booking2_ms/Booking2MsApplication.java
@@ -0,0 +1,13 @@
+package com.example.booking2_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Booking2MsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Booking2MsApplication.class, args);
+	}
+
+}
---
+++ booking2-ms/src/test/java/com/example/booking2_ms/Booking2MsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booking2_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Booking2MsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
